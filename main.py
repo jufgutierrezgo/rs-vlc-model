962385a2bca43d512ac120c63cceace822459521
juanfg2 <jufgutierrezgo@unal.edu.co>
2023-03-15 02:57:55 -0500

    Main with TX and SFC test

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+#Import module Transmitter
+from transmitter import Transmitter as Transmitter
+#Import module Surface
 from surface import Surface as Surface
+
+
+
+transmitter = Transmitter(
+        "Led1",
+        position=[2.5, 2.5, 3],
+        normal=[0, 0, -1],
+        mlambert=1,
+        wavelengths=[620, 530, 475],
+        fwhm=[20, 45, 20],
+        modulation='ieee16',
+        luminous_flux=5000
+    )
+transmitter.plot_spd_normalized
 
 surface = Surface(
     name="surfacePlaster",
